fa64d98d79b1dca66dde8dc4c22f4620fd9091c8
Adrians Riekstins 201RBC027 <124619408+RiekstinsA@users.noreply.github.com>
2023-03-04 20:38:12 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -16,10 +16,10 @@ def compute_height(n, parents):
 
     def height(node):
         if not adj_list[node]:
+            # base case: leaf node
             return 1
         else:
             return 1 + max([height(child) for child in adj_list[node]])
-
 
     return height(root)
 
@@ -33,10 +33,6 @@ def main():
 
     print(compute_height(n, parents))
 
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
