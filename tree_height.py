153894bab32cf514b612953939b01baf9f214078
Adrians Riekstins 201RBC027 <124619408+RiekstinsA@users.noreply.github.com>
2023-03-05 21:55:58 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -3,7 +3,9 @@
 import sys
 import threading
 
+
 def compute_height(n, parents):
+
     adj_list = [[] for _ in range(n)]
     for i in range(n):
         parent = parents[i]
@@ -11,24 +13,28 @@ def compute_height(n, parents):
             root = i
         else:
             adj_list[parent].append(i)
-    def max_height(node):
+
+    def height(node):
         if not adj_list[node]:
             return 1
         else:
-            return 1 + max([max_height(child) for child in adj_list[node]])
-    return max_height(root)
+            return 1 + max([height(child) for child in adj_list[node]])
+
+    return height(root)
+
 
 def main():
     inputstr = input().strip()
-    inputval = inputstr.split('\n')
+    inputval = inputstr.split('\\r\\n')
 
-    if "I" in inputval[0]:
+    if "I" in inputval:
         n = int(inputval[1])
         parents = list(map(int, inputval[2].split()))
 
-    elif "F" in inputval[0]:
-        filename = inputval[1].strip()
+    elif "F" in inputval:
+        filename = inputstr.split('\\r\\n')[1]
         testfolder = "./test/" + filename
+        
         with open(testfolder, mode='r') as file:
             text = file.read().strip()
             n, parents = int(text.split('\n')[0]), list(map(int, text.split('\n')[1].split()))
