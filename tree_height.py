017ee20362d57f154eb8ce3db0cfdbf9350afed1
Adrians Riekstins 201RBC027 <124619408+RiekstinsA@users.noreply.github.com>
2023-03-05 21:46:10 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -4,50 +4,42 @@ import sys
 import threading
 
 
-def compute_height(nodes, parents):
+def compute_height(n, parents):
 
-    adj_list = [[] for _ in range(nodes)]
-    for i in range(nodes):
+    adj_list = [[] for _ in range(n)]
+    for i in range(n):
         parent = parents[i]
         if parent == -1:
             root = i
         else:
             adj_list[parent].append(i)
 
-    def height(node):
+    def max_height(node):
         if not adj_list[node]:
             return 1
         else:
-            return 1 + max([height(child) for child in adj_list[node]])
+            return 1 + max([max_height(child) for child in adj_list[node]])
 
-    return height(root)
+    return max_height(root)
 
 
 def main():
-    input_i_f = input()
+    inputstr = input().strip()
+    inputval = inputstr.split('\\r\\n')
 
-    if input_i_f == "I":
-        nodes = int(input())
-        parents = list(map(int, input().split()))
+    if "I" in inputval:
+        n = int(inputval[1])
+        parents = list(map(int, inputval[2].split()))
 
-    elif input_i_f == "F":
-        filename = input()
-        if "a" in filename:
-            print("invalid file name (a)")
-            return
-        try:
-            testfolder = "./test/" + filename
-            with open(testfolder, 'r') as f:
-                nodes = int(f.readline().strip())
-                parents = list(map(int, f.readline().strip().split()))
-        except FileNotFoundError:
-            print("file not found")
-            return
-    else:
-        print("invalid input type")
-        return
+    elif "F" in inputval:
+        filename = inputstr.split('\\r\\n')[1]
+        testfolder = "./test/" + filename
 
-    print(compute_height(nodes, parents))
+        with open (testfolder, mode = 'r') as file:
+            text = file.read().strip()
+            n, parents = int(text.split('\n')[0]), list(map(int, text.split('\n')[1].split()))
+
+    print(compute_height(n, parents))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
