76d30f856f2ec5bb7d0ad7129686cc7aaa3362d9
Adrians Riekstins 201RBC027 <124619408+RiekstinsA@users.noreply.github.com>
2023-03-05 21:14:00 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -3,59 +3,55 @@
 import sys
 import threading
 
-def compute_height(n, parents):
-    if n == 0:
-        return 0
 
-    depths = [0] * n
+def compute_height(nodes, parents):
 
-    root = None
-    for i in range(n):
-        if parents[i] == -1:
+    adj_list = [[] for _ in range(nodes)]
+    for i in range(nodes):
+        parent = parents[i]
+        if parent == -1:
             root = i
         else:
-            depths[i] = depths[parents[i]] + 1
+            adj_list[parent].append(i)
 
-    max_depth = max(depths)
-    return max_depth
+    def height(node):
+        if not adj_list[node]:
+            return 1
+        else:
+            return 1 + max([height(child) for child in adj_list[node]])
+
+    return height(root)
+
 
 def main():
-    input_string = input().strip()
-    input_values = input_string.split('\\r\\n')
-    
-    # Check if input has correct format
-    if len(input_values) < 2:
-        print("Error: input is not well-formed")
-        return
-    
-    if input_values[0] == "I":
-        if len(input_values) < 3:
-            print("Error: input is not well-formed")
+    input_i_f = input("I vai F: ")
+
+    if input_i_f == "I":
+        nodes = int(input())
+        parents = list(map(int, input().split()))
+
+    elif input_i_f == "F":
+        filename = input("file name: ")
+        if "a" in filename:
+            print("invalid file name (a)")
             return
-        n = int(input_values[1])
-        parents = list(map(int, input_values[2].split()))
-    elif input_values[0] == "F":
-        if len(input_values) < 2:
-            print("Error: input is not well-formed")
+        try:
+            testfolder = "./test/" + filename
+            with open(testfolder, 'r') as f:
+                nodes = int(f.readline().strip())
+                parents = list(map(int, f.readline().strip().split()))
+        except FileNotFoundError:
+            print("file not found")
             return
-        filename = input_values[1]
-        testfolder = "./test/" + filename
-        
-        with open(testfolder, mode='r') as file:
-            text = file.read().strip()
-            n, parents = int(text.split('\n')[0]), list(map(int, text.split('\n')[1].split()))
     else:
-        print("Error: invalid input")
+        print("invalid input type")
         return
-    
-    sys.setrecursionlimit(10**7)
-    height = compute_height(n, parents)
-    print(height)
-    
+
+    print(compute_height(nodes, parents))
+
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
